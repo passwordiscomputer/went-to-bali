a9e88b26ff3fd4cbc14ebe36dd87f0f5dfd63f75
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-05-07 19:49:50 -0700

    add edit products page and functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,18 @@ class ProductsController < ApplicationController
     end
   end
 
-  
+  def edit
+   @product = Product.find(params[:id])
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to edit_product_path(@product)
+    else
+      render :edit
+    end
+  end
 
   private
   def product_params

b3db5f0edd7e04841141debdfefb6f775afafd1b
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-05-07 20:06:13 -0700

    add admin to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,21 @@
 class Seed
 
   def self.begin
+    Product.destroy_all
+    User.destroy_all
     seed = Seed.new
     seed.generate_products
+    seed.generate_admin
   end
 
   def generate_products
     20.times do |i|
       Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
     end
+  end
+
+  def generate_admin
+    User.create(name: "admin", email: "admin@admin.com", password_digest: "password", admin: true)
   end
 end
 
